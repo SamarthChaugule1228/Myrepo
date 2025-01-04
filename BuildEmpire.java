85f5eb1d52d3be3aec0187e226297de84fd687c0
SamarthChaugule1228 <chaugulesamarth12@gmail.com>
2025-01-04 14:23:38 +0530

    Build Empire is Started with basic Login

---
+++ BuildEmpire.java
@@ -0,0 +1,11 @@
+public class BuildEmpire {
+
+    public static void main(String[] args) {
+        //Login Module with Authintication ---> Master work Branch 
+        System.out.println("___________Login Module __________");
+        System.out.println("Name :");
+        System.out.println("Password: ");
+        System.out.println("___________________________________");
+        
+    }
+}
